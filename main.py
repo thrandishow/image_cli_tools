d0a0f54c1056dc31d73235ff40dc6fb63dadfc31
voronin_k <thrandi230807@gmail.com>
2026-01-31 18:09:21 +0300

    Added resize endpoint

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+from typing import Optional
 import typer
 from PIL import Image
 
@@ -15,6 +17,35 @@ def info(path: Path = typer.Argument(..., help="Path to image", exists=True)):
             typer.echo(f"Image name: {typer.style(im.filename, fg="green")}")
             typer.echo(f"Image format: {typer.style(im.format, fg="green")}")
             typer.echo(f"Image size: {typer.style(f"{height}x{width}", fg="green")}")
+
+    except Exception as e:
+        typer.secho(e, err=True, fg="red")
+        typer.Exit(1)
+
+
+@app.command()
+def resize_image(
+    height: int,
+    width: int,
+    image_name: str,
+    directory: Optional[Path] = None,
+    path: Path = typer.Argument(..., help="Path to image", exists=True),
+):
+    try:
+        with Image.open(path) as im:
+            size = (height, width)
+            resized_image = im.resize(size)
+
+            if directory:
+                resized_image.save(f"{directory}/{image_name}")
+                typer.echo(
+                    f"Resized image saved \nin {typer.style(directory,fg="green")} directory with {typer.style(image_name,fg="blue")} name"
+                )
+            else:
+                resized_image.save(image_name)
+                typer.echo(
+                    f"Resized image saved in {typer.style("current", fg="magenta")} directory"
+                )
 
     except Exception as e:
         typer.secho(e, err=True, fg="red")
