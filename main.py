fa68c5b2d00fe2fc2c89166a19436ca435de3c6f
voronin_k <thrandi230807@gmail.com>
2026-02-01 16:40:06 +0300

    Added docstrings,  replaced width and height options

--- main.py
+++ main.py
@@ -4,52 +4,79 @@ from typing import Optional
 import typer
 from PIL import Image
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False)
 
 
 @app.command()
 def info(path: Path = typer.Argument(..., help="Path to image", exists=True)):
-    "Show image information"
+    """Shows information about the image
 
+    Args:
+        path (Path, required): Path to image. Defaults to typer.Argument(..., help="Path to image", exists=True).
+
+    Raises:
+        typer.Exit:  Exit the program
+    """
     try:
         with Image.open(path) as im:
-            height, width = im.size
+            width, height = im.size
             typer.echo(f"Image name: {typer.style(im.filename, fg="green")}")
             typer.echo(f"Image format: {typer.style(im.format, fg="green")}")
-            typer.echo(f"Image size: {typer.style(f"{height}x{width}", fg="green")}")
+            typer.echo(f"Image size: {typer.style(f"{width}x{height}", fg="green")}")
 
     except Exception as e:
         typer.secho(e, err=True, fg="red")
-        typer.Exit(1)
+        raise typer.Exit(1)
 
 
 @app.command()
 def resize_image(
-    height: int,
-    width: int,
-    image_name: str,
-    directory: Optional[Path] = None,
     path: Path = typer.Argument(..., help="Path to image", exists=True),
+    width: int = typer.Argument(..., help="Width of image"),
+    height: int = typer.Argument(..., help="Height of image"),
+    output_name: Optional[str] = typer.Option(
+        None, "--name", "-n", help="Resized image name"
+    ),
+    output_dir: Optional[Path] = typer.Option(
+        None, "--dir", "-d", help="Path to save resized image"
+    ),
 ):
+    """Resizes image
+
+    Args:
+        path (Path, required): Path to image. Defaults to typer.Argument(..., help="Path to image", exists=True).
+        width (int, required): Width of image. Defaults to typer.Argument(..., help="Width of image").
+        height (int, required): Height of image. Defaults to typer.Argument(..., help="Height of image").
+        output_name (Optional[str], optional): Output name of resized image. Defaults to typer.Option( None, "--name", "-n", help="Resized image name" ).
+        output_dir (Optional[Path], optional): Output dir of resized image. Defaults to typer.Option( None, "--dir", "-d", help="Path to save resized image" ).
+
+    Raises:
+        typer.Exit: Exit the program
+    """
+
     try:
         with Image.open(path) as im:
-            size = (height, width)
+            size = (width, height)
             resized_image = im.resize(size)
 
-            if directory:
-                resized_image.save(f"{directory}/{image_name}")
-                typer.echo(
-                    f"Resized image saved \nin {typer.style(directory,fg="green")} directory with {typer.style(image_name,fg="blue")} name"
-                )
+            if output_name is None:
+                output_name = f"{path.stem}_resized{path.suffix}"
+
+            if output_dir:
+                output_dir.mkdir(parents=True, exist_ok=True)
+                output_path = output_dir / output_name
+
             else:
-                resized_image.save(image_name)
-                typer.echo(
-                    f"Resized image saved in {typer.style("current", fg="magenta")} directory"
-                )
+                output_path = Path(output_name)
+
+            resized_image.save(output_path)
+            typer.echo(
+                f"Resized image saved to: {typer.style(str(output_path), fg=typer.colors.GREEN)}"
+            )
 
     except Exception as e:
         typer.secho(e, err=True, fg="red")
-        typer.Exit(1)
+        raise typer.Exit(1)
 
 
 if __name__ == "__main__":
