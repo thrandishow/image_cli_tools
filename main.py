99cbf56b431871e526fbefe16fb5c6b1a7bf7d7c
voronin_k <thrandi230807@gmail.com>
2026-02-02 19:28:31 +0300

    New function and utils with functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
 from pathlib import Path
 from typing import Optional
 import typer
 from PIL import Image
+from .utils import print_exception, get_output_name, get_output_path
 
 app = typer.Typer(add_completion=False)
 
@@ -23,10 +23,10 @@ def info(path: Path = typer.Argument(..., help="Path to image", exists=True)):
             typer.echo(f"Image name: {typer.style(im.filename, fg="green")}")
             typer.echo(f"Image format: {typer.style(im.format, fg="green")}")
             typer.echo(f"Image size: {typer.style(f"{width}x{height}", fg="green")}")
+            typer.echo(f"Image mode: {typer.secho(im.mode, fg="green")}")
 
     except Exception as e:
-        typer.secho(e, err=True, fg="red")
-        raise typer.Exit(1)
+        print_exception(e)
 
 
 @app.command()
@@ -56,18 +56,10 @@ def resize_image(
 
     try:
         with Image.open(path) as im:
-            size = (width, height)
-            resized_image = im.resize(size)
+            resized_image = im.resize(size=(width, height))
 
-            if output_name is None:
-                output_name = f"{path.stem}_resized{path.suffix}"
-
-            if output_dir:
-                output_dir.mkdir(parents=True, exist_ok=True)
-                output_path = output_dir / output_name
-
-            else:
-                output_path = Path(output_name)
+            output_name = get_output_name(output_name, path)
+            output_path = get_output_path(path, output_name)
 
             resized_image.save(output_path)
             typer.echo(
@@ -75,8 +67,35 @@ def resize_image(
             )
 
     except Exception as e:
-        typer.secho(e, err=True, fg="red")
-        raise typer.Exit(1)
+        print_exception(e)
+
+
+@app.command()
+def opitimize_image(
+    path: Path = typer.Argument(..., help="Path to image", exists=True),
+    width: Optional[int] = typer.Option(None, "--width", "-w", help="Width of image"),
+    height: Optional[int] = typer.Option(
+        None, "--height", "-H", help="Height of image"
+    ),
+    output_name: Optional[str] = typer.Option(
+        None, "--name", "-n", help="Output name of image"
+    ),
+    output_dir: Optional[Path] = typer.Option(
+        None, "--dir", "-d", help="Path to save optimized image"
+    ),
+):
+    try:
+        with Image.open(path) as im:
+            output_name = get_output_name(output_name, path)
+            output_path = get_output_path(output_dir, output_name)
+
+            if (width or height) is not None:
+                width = width if width is not None else im.width
+                height = height if height is not None else im.height
+                im = im.resize(size=(width, height))
+        # TODO: Add optimization
+    except Exception as e:
+        print_exception(e)
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,23 @@
+import typer
+from pathlib import Path
+
+
+def print_exception(e: Exception):
+    typer.secho(e, err=True, fg="red")
+    raise typer.Exit(1)
+
+
+def get_output_name(output_name: None | str, path: Path):
+    if output_name is None:
+        output_name = f"{path.stem}_resized{path.suffix}"
+    return output_name
+
+
+def get_output_path(output_dir: Path | None, output_name: str):
+    if output_dir is not None:
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_path = output_dir / output_name
+
+    else:
+        output_path = Path(output_name)
+    return output_path
