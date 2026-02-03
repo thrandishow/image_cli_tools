48860d3102748371442420cf92e0b22fa25b7ddb
voronin_k <thrandi230807@gmail.com>
2026-02-03 19:10:46 +0300

    Calculates benefit in size

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from typing import Optional
 import typer
 from PIL import Image
-from utils import print_exception, get_output_name, get_output_path
+import utils
 
 app = typer.Typer(add_completion=False)
 
@@ -26,7 +26,7 @@ def info(path: Path = typer.Argument(..., help="Path to image", exists=True)):
             typer.echo(f"Image mode: {typer.secho(im.mode, fg="green")}")
 
     except Exception as e:
-        print_exception(e)
+        utils.print_exception(e)
 
 
 @app.command()
@@ -58,8 +58,8 @@ def resize_image(
         with Image.open(path) as im:
             resized_image = im.resize(size=(width, height))
 
-            output_name = get_output_name(output_name, path)
-            output_path = get_output_path(path, output_name)
+            output_name = utils.get_output_name(output_name, path)
+            output_path = utils.get_output_path(output_dir, output_name)
 
             resized_image.save(output_path)
             typer.echo(
@@ -67,7 +67,7 @@ def resize_image(
             )
 
     except Exception as e:
-        print_exception(e)
+        utils.print_exception(e)
 
 
 @app.command()
@@ -95,8 +95,9 @@ def optimize_image(
     """
     try:
         with Image.open(path) as im:
-            output_name = get_output_name(output_name, path)
-            output_path = get_output_path(output_dir, output_name)
+            primal_size: float = round(path.stat().st_size / 1024**2, 2)
+            output_name = utils.get_output_name(output_name, path)
+            output_path = utils.get_output_path(output_dir, output_name)
 
             if (width or height) is not None:
                 width = width if width is not None else im.width
@@ -104,8 +105,14 @@ def optimize_image(
                 im = im.resize(size=(width, height))
 
             im.save(output_path, optimize=True, quality=95)
+            end_size: float = round(output_path.stat().st_size / 1024**2, 2)
+            size_benefit_percents = utils.calculate_benefit(primal_size, end_size)
+            typer.echo(
+                f"Optimized image saved to: {typer.style(str(output_path), fg=typer.colors.GREEN)} {typer.style(f"{size_benefit_percents}% ⬆️",fg="magenta")}"
+            )
+
     except Exception as e:
-        print_exception(e)
+        utils.print_exception(e)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -13,11 +13,15 @@ def get_output_name(output_name: None | str, path: Path):
     return output_name
 
 
-def get_output_path(output_dir: Path | None, output_name: str):
+def get_output_path(output_dir: Path | None, output_name: str) -> Path:
     if output_dir is not None:
         output_dir.mkdir(parents=True, exist_ok=True)
-        output_path = output_dir / output_name
+        output_path = Path(output_dir / output_name)
 
     else:
         output_path = Path(output_name)
     return output_path
+
+
+def calculate_benefit(primal_size: float, end_size: float) -> float:
+    return round(100 - (end_size * 100 / primal_size), 2)
